096f4c23392ac620ee7e4acc922f42d0ff273abe
JulesTopart <jules.topart@gmail.com>
2019-03-08 19:16:52 +0100

    Update botlystudio.js

--- botlystudio/botlystudio.js
+++ botlystudio/botlystudio.js
@@ -110,7 +110,7 @@ BotlyStudio.ideButtonLastAction = function () {
 
 BotlyStudio.saveCanvas = function(){
   
-  Renderer.spawnSprite("botlystudio/sprites/copyright.png");
+  path = "botlystudio/sprites/copyright.png";
 
   var cpyright = new Image();
   cpyright.src = path;
